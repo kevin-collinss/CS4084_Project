6be746d5906fb37f5b0712cb6935c525d5d6173b
caoimhe <caoimh3cahill@gmail.com>
2024-03-24 18:51:39 +0000

    Fixing home layout

--- app/src/main/java/ie/ul/ulthrift/fragments/HomeFragment.java
+++ app/src/main/java/ie/ul/ulthrift/fragments/HomeFragment.java
@@ -69,7 +69,7 @@ public class HomeFragment extends Fragment {
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
         List<SlideModel> slideModels = new ArrayList<>();
 
-        slideModels.add(new SlideModel(R.drawable.shoes, "Discount on Shoes", ScaleTypes.CENTER_CROP));
+        slideModels.add(new SlideModel(R.drawable.shoes, "Shoes", ScaleTypes.CENTER_CROP));
         slideModels.add(new SlideModel(R.drawable.mens_clothing, "Mens Clothing", ScaleTypes.CENTER_CROP));
         slideModels.add(new SlideModel(R.drawable.womens_clothing, "Womens Clothing", ScaleTypes.CENTER_CROP));
 
