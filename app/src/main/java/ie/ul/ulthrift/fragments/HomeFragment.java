ac6aeafcf2a8bc35112c6f0b86deb21e9a6ffc37
caoimhe <caoimh3cahill@gmail.com>
2024-03-24 14:39:12 +0000

    Adding categories to home screen using firebasestore database and storage

--- app/src/main/java/ie/ul/ulthrift/fragments/HomeFragment.java
+++ app/src/main/java/ie/ul/ulthrift/fragments/HomeFragment.java
@@ -2,8 +2,12 @@ package ie.ul.ulthrift.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +15,30 @@ import android.view.ViewGroup;
 import com.denzcoskun.imageslider.ImageSlider;
 import com.denzcoskun.imageslider.constants.ScaleTypes;
 import com.denzcoskun.imageslider.models.SlideModel;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import ie.ul.ulthrift.R;
+import ie.ul.ulthrift.adaptors.CategoryAdaptor;
+import ie.ul.ulthrift.models.CategoryModel;
 
 public class HomeFragment extends Fragment {
+
+    RecyclerView catRecyclerview;
+
+    //Category recyclerview
+    CategoryAdaptor categoryAdaptor;
+    List<CategoryModel> categoryModelList;
+
+    //FireStore
+    FirebaseFirestore db;
+
 
     public HomeFragment() {
         // Required empty public constructor
@@ -27,8 +48,13 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
+        catRecyclerview = root.findViewById(R.id.rec_category);
+
+        db = FirebaseFirestore.getInstance();
+
 
         //Image slider
         ImageSlider imageSlider = root.findViewById(R.id.image_slider);
@@ -39,6 +65,36 @@ public class HomeFragment extends Fragment {
         slideModels.add(new SlideModel(R.drawable.womens_clothing, "Womens Clothing", ScaleTypes.CENTER_CROP));
 
         imageSlider.setImageList(slideModels);
+
+        //Category
+        catRecyclerview.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryModelList = new ArrayList<>();
+        categoryAdaptor = new CategoryAdaptor(getContext(),categoryModelList);
+        catRecyclerview.setAdapter(categoryAdaptor);
+
+
+        db.collection("Category")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                            CategoryModel categoryModel = document.toObject(CategoryModel.class);
+                            categoryModelList.add(categoryModel);
+                            categoryAdaptor.notifyDataSetChanged();
+                            Log.d("FireStore", document.getId() + " => " + document.getData());
+                        }
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w("Firestore", "Error getting documents.", e);
+                    }
+                });
+
+
+
 
         return root;
     }
